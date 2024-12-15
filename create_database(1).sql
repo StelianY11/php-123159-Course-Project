0e9aa8525b74c2313e0a3fa940fcb7abbc14281d
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-12-15 20:11:47 +0200

    .

--- create_database(1).sql
+++ create_database(1).sql
@@ -1,4 +1,4 @@
 CREATE DATABASE books;
-CREATE USER 'books_user'@'localhost' IDENTIFIED BY '123456';
+CREATE USER 'books_user'@'localhost' IDENTIFIED BY '54321';
 GRANT ALL PRIVILEGES ON books.* TO 'books_user'@'localhost';
 FLUSH PRIVILEGES;
